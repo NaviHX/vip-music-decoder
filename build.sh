b7523a212e7e738439a4ea67e9b1ec14d2a0f327
navihx <932942142@qq.com>
2021-01-16 17:15:09 +0800

    重写build脚本

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
 mkdir build
 cd ./build
-cmake ..
+
+FIND=$(ldconfig -p | grep libtag)
+if [ "$FIND" = "" ];
+then
+    say="CANNOT find taglib. Only build qmcdump."
+    cmake ..
+else
+    say="Taglib Found. Build both qmcdump and ncmdump."
+    cmake .. -DFIND=true
+fi
+
 make
+
+cd ..
+mv ./build/*dump .
+rm -r ./build
+echo "$say"
